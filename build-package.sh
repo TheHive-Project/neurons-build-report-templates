ab83201412892fcad68a57de8af819690c9968c1
Jérôme Leonard <jerome@thehive-project.org>
2020-10-15 18:49:55 +0200

    test

--- build-package.sh
+++ build-package.sh
@@ -2,7 +2,13 @@
 
 build_template_package() {
     cd thehive-templates && zip -r ../report-templates.zip *
-    cd .. && ls -la .
+    cd ..
+    ls -la report-templates.zip
+    ls -la analyzers/analyzers.json
+    ls -la analyzers/analyzers-stable.json
+    ls -la responders/responders.json
+    ls -la responders/responders-stable.json
+
 }
 
 build_template_package
