7ee8b2f47d816892e175dc7cadf64de8827b8c43
Jérôme Leonard <jerome@thehive-project.org>
2020-10-16 10:03:41 +0200

    fix typo

--- build-package.sh
+++ build-package.sh
@@ -3,7 +3,7 @@
 build_template_package() {
     cd thehive-templates && zip -r ../analyzers/report-templates.zip *
     cd ..
-    ls -la analyers/report-templates.zip
+    ls -la analyzers/report-templates.zip
     ls -la analyzers/analyzers.json
     ls -la analyzers/analyzers-stable.json
     ls -la responders/responders.json
